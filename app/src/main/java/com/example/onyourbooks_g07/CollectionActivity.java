7a02d481f89d6fc5d5a79b04ddfb3300ff56efc5
2001bisthebest <baipor2556@gmail.com>
2023-11-26 23:55:46 +0700

    create intent mainactivity to collectionactivity

--- app/src/main/java/com/example/onyourbooks_g07/CollectionActivity.java
+++ app/src/main/java/com/example/onyourbooks_g07/CollectionActivity.java
@@ -1,4 +1,25 @@
 package com.example.onyourbooks_g07;
 
-public class CollectionActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollectionActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.column_list_collection);
+
+        Button back_btn = findViewById(R.id.back_btn);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CollectionActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/onyourbooks_g07/MainActivity.java
+++ app/src/main/java/com/example/onyourbooks_g07/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.onyourbooks_g07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,11 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button col_btn = findViewById(R.id.button_collection);
+        Button col_btn = findViewById(R.id.collection);
         col_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, CollectionActivity.class);
+                startActivity(intent);
             }
         });
     }
