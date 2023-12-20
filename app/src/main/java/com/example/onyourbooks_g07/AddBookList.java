f12a935f89f73add8c0d38a51b1e02366e645e77
Nicharee <eerahcin44@gmail.com>
2023-12-21 01:35:58 +0700

    make dialog button,change color,portrait

--- app/src/main/java/com/example/onyourbooks_g07/AddBookList.java
+++ app/src/main/java/com/example/onyourbooks_g07/AddBookList.java
@@ -1,9 +1,12 @@
 package com.example.onyourbooks_g07;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,13 @@ public class AddBookList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_book_list);
+
+        Resources res = getResources();
+        final EditText date = findViewById(R.id.editTextDate2);
+        final EditText name = findViewById(R.id.editTextText4);
+        final EditText price = findViewById(R.id.editprice);
+        final Button submit = findViewById(R.id.Button_submit);
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_collection_activity, menu);
--- app/src/main/java/com/example/onyourbooks_g07/MainActivity.java
+++ app/src/main/java/com/example/onyourbooks_g07/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.onyourbooks_g07;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button col_btn = findViewById(R.id.collection);
+
+        ImageView info_btn = findViewById(R.id.info_btn); //ปุ่มดูชื่อสมาชิก
+         info_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Toast.makeText(MainActivity.this, "Member Information ", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("CS361 \n MEMBER INFORMATION");
+                builder.setMessage("Woranut Kitsirisakulwong 6309681366 \n Nicharee Chuachart 6309681648 ");
+                builder.setPositiveButton("CLOSE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // ปุ่มปิด
+                    }
+                });
+                // Dialog
+                builder.show();
+            }
+        });
+         //งงวะแงงกดส
+
+
         col_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,4 +50,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-}
+    }
